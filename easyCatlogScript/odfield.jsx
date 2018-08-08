0b20177e39c4dc5cceb035338a48a072c54deb26
Kevin <kevin.noseworthy@stjoseph.com>
2018-08-08 12:02:22 -0400

    odfield

--- easyCatlogScript/odfield.jsx
+++ easyCatlogScript/odfield.jsx
@@ -19,7 +19,36 @@ switch(layout){
     case 'FrontCoverLB':
     layoutAry.push('frontcover');
     break;
+    default:
+    layoutAry.push(layout);
+    break;
 }
+var orgPrice = myRecord['price'];
+var orgPriceLb = myRecord['pricelb'];
+var price = orgPrice;
+var enddigt = "US";
+
+if(orgPriceLb != "00¢"){
+    price = orgPriceLb;
+    enddigt = "LB"
+}
+if(myRecord['uos'] != "" && myRecord['uos'].indexOf("/")){
+    layoutAry.push("x/x");
+}
+var cents = price.indexOf("¢") >0 ? "dollar" : "cent";
+layoutAry.push(cents);
+
+var decimals = price.replace(/[^0-9\.]+/g, '') % 1;
+
+if(decimals != 0){
+    var split = orgPrice.split(".");
+    layoutAry.push(split[0].length);
+}
+else{    
+    layoutAry.push(orgPrice.length);
+}
+
+
 var obj = layoutAry.join(" ").replace("  "," ");
 }catch(e){
     var obj = "Script Error:"+ e.message;
